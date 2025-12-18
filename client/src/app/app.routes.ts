709a135ae8dd8a6f1853e8dd25a8f83dbaeb77e8
Alvin N <109256320+AlvinNjenga@users.noreply.github.com>
2025-12-18 22:18:57 +0000

    Added login guards

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
+import { loginGuard } from './guards/login-guard';
 
 export const routes: Routes = [
     {
         path: "register",
         loadComponent: () => import('./register/register').then((x) => x.Register),
+        canActivate: [loginGuard]
     },
     {
         path: 'login',
         loadComponent: () => import('./login/login').then((x) => Login),
+        canActivate: [loginGuard]
     }
 ];
